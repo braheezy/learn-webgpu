9f5fb07d9379feffea2b17fbde69dfb7c4d79cb1
braheezy <11700503+braheezy@users.noreply.github.com>
2025-02-25 21:37:34 -0800

    Hello triangle!

--- src/App.zig
+++ src/App.zig
@@ -2,11 +2,31 @@ const std = @import("std");
 const zglfw = @import("zglfw");
 const zgpu = @import("zgpu");
 
+const shader_source =
+    \\@vertex
+    \\fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4f {
+    \\    var p = vec2f(0.0, 0.0);
+    \\    if (in_vertex_index == 0u) {
+    \\        p = vec2f(-0.5, -0.5);
+    \\    } else if (in_vertex_index == 1u) {
+    \\        p = vec2f(0.5, -0.5);
+    \\    } else {
+    \\        p = vec2f(0.0, 0.5);
+    \\    }
+    \\    return vec4f(p, 0.0, 1.0);
+    \\}
+    \\@fragment
+    \\fn fs_main() -> @location(0) vec4f {
+    \\    return vec4f(0.0, 0.4, 1.0, 1.0);
+    \\}
+;
+
 const App = @This();
 
 allocator: std.mem.Allocator,
 window: *zglfw.Window,
 gfx: *zgpu.GraphicsContext,
+pipeline: zgpu.RenderPipelineHandle,
 
 pub fn init(allocator: std.mem.Allocator) !*App {
     try zglfw.init();
@@ -34,10 +54,12 @@ pub fn init(allocator: std.mem.Allocator) !*App {
     );
 
     const app = try allocator.create(App);
+    const pipeline = createPipeline(gctx);
     app.* = App{
         .allocator = allocator,
         .window = window,
         .gfx = gctx,
+        .pipeline = pipeline,
     };
     return app;
 }
@@ -73,8 +95,14 @@ pub fn run(self: *App) void {
         };
 
         const pass = encoder.beginRenderPass(render_pass_info);
+        const pipeline = self.gfx.lookupResource(self.pipeline) orelse unreachable;
+
+        pass.setPipeline(pipeline);
+        pass.draw(3, 1, 0, 0);
+
         pass.end();
         pass.release();
+
         const command_buffer = encoder.finish(null);
         defer command_buffer.release();
 
@@ -83,4 +111,51 @@ pub fn run(self: *App) void {
 
         self.window.swapBuffers();
     }
+}
+
+fn createPipeline(gctx: *zgpu.GraphicsContext) zgpu.RenderPipelineHandle {
+    const shader_module = zgpu.createWgslShaderModule(gctx.device, shader_source, "main");
+    defer shader_module.release();
+
+    const pipeline_layout = zgpu.PipelineLayoutHandle.nil;
+
+    const color_targets = [_]zgpu.wgpu.ColorTargetState{.{
+        .format = .bgra8_unorm,
+        .blend = &zgpu.wgpu.BlendState{
+            .color = .{
+                .src_factor = .src_alpha,
+                .dst_factor = .one_minus_src_alpha,
+                .operation = .add,
+            },
+            .alpha = .{
+                .src_factor = .zero,
+                .dst_factor = .one,
+                .operation = .add,
+            },
+        },
+        .write_mask = .all,
+    }};
+
+    const pipeline_desc = zgpu.wgpu.RenderPipelineDescriptor{
+        .vertex = .{
+            .module = shader_module,
+            .entry_point = "vs_main",
+            .buffer_count = 0,
+            .buffers = null,
+        },
+        .primitive = .{
+            .topology = .triangle_list,
+            .front_face = .ccw,
+            .cull_mode = .none,
+        },
+        .fragment = &zgpu.wgpu.FragmentState{
+            .module = shader_module,
+            .entry_point = "fs_main",
+            .target_count = color_targets.len,
+            .targets = &color_targets,
+        },
+        .depth_stencil = null,
+    };
+
+    return gctx.createRenderPipeline(pipeline_layout, pipeline_desc);
 }
