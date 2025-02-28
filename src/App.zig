6d7347b1380c3701616d5fffa1d07432005a7e22
braheezy <11700503+braheezy@users.noreply.github.com>
2025-02-28 14:51:17 -0800

    A first Vertex Attribute

--- src/App.zig
+++ src/App.zig
@@ -9,16 +9,8 @@ const Context = struct {
 
 const shader_source =
     \\@vertex
-    \\fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4f {
-    \\    var p = vec2f(0.0, 0.0);
-    \\    if (in_vertex_index == 0u) {
-    \\        p = vec2f(-0.5, -0.5);
-    \\    } else if (in_vertex_index == 1u) {
-    \\        p = vec2f(0.5, -0.5);
-    \\    } else {
-    \\        p = vec2f(0.0, 0.5);
-    \\    }
-    \\    return vec4f(p, 0.0, 1.0);
+    \\fn vs_main(@location(0) in_vertex_position: vec2f) -> @builtin(position) vec4f {
+    \\    return vec4f(in_vertex_position, 0.0, 1.0);
     \\}
     \\@fragment
     \\fn fs_main() -> @location(0) vec4f {
@@ -26,12 +18,24 @@ const shader_source =
     \\}
 ;
 
+const vertex_data = [_]f32{
+    -0.5,  -0.5,
+    0.5,   -0.5,
+    0.0,   0.5,
+
+    -0.55, -0.5,
+    -0.05, 0.5,
+    -0.55, 0.5,
+};
+
 const App = @This();
 
 allocator: std.mem.Allocator,
 window: *zglfw.Window,
 gfx: *zgpu.GraphicsContext,
 pipeline: zgpu.RenderPipelineHandle,
+vertex_buffer: zgpu.wgpu.Buffer = undefined,
+vertex_count: u32,
 
 pub fn init(allocator: std.mem.Allocator) !*App {
     try zglfw.init();
@@ -40,23 +44,26 @@ pub fn init(allocator: std.mem.Allocator) !*App {
 
     const window = try zglfw.createWindow(640, 480, "Learn WebGPU", null);
 
-    const gctx = try zgpu.GraphicsContext.create(
-        allocator,
-        .{
-            .window = window,
-            .fn_getTime = @ptrCast(&zglfw.getTime),
-            .fn_getFramebufferSize = @ptrCast(&zglfw.Window.getFramebufferSize),
+    const gctx = try zgpu.GraphicsContext.create(allocator, .{
+        .window = window,
+        .fn_getTime = @ptrCast(&zglfw.getTime),
+        .fn_getFramebufferSize = @ptrCast(&zglfw.Window.getFramebufferSize),
 
-            // optional fields
-            .fn_getWin32Window = @ptrCast(&zglfw.getWin32Window),
-            .fn_getX11Display = @ptrCast(&zglfw.getX11Display),
-            .fn_getX11Window = @ptrCast(&zglfw.getX11Window),
-            .fn_getWaylandDisplay = @ptrCast(&zglfw.getWaylandDisplay),
-            .fn_getWaylandSurface = @ptrCast(&zglfw.getWaylandWindow),
-            .fn_getCocoaWindow = @ptrCast(&zglfw.getCocoaWindow),
+        // optional fields
+        .fn_getWin32Window = @ptrCast(&zglfw.getWin32Window),
+        .fn_getX11Display = @ptrCast(&zglfw.getX11Display),
+        .fn_getX11Window = @ptrCast(&zglfw.getX11Window),
+        .fn_getWaylandDisplay = @ptrCast(&zglfw.getWaylandDisplay),
+        .fn_getWaylandSurface = @ptrCast(&zglfw.getWaylandWindow),
+        .fn_getCocoaWindow = @ptrCast(&zglfw.getCocoaWindow),
+    }, .{ .required_limits = &zgpu.wgpu.RequiredLimits{
+        .limits = .{
+            .max_vertex_attributes = 1,
+            .max_vertex_buffers = 1,
+            .max_buffer_size = 6 * 2 * @sizeOf(f32),
+            .max_vertex_buffer_array_stride = 2 * @sizeOf(f32),
         },
-        .{}, // default context creation options
-    );
+    } });
 
     const app = try allocator.create(App);
     const pipeline = createPipeline(gctx);
@@ -65,11 +72,14 @@ pub fn init(allocator: std.mem.Allocator) !*App {
         .window = window,
         .gfx = gctx,
         .pipeline = pipeline,
+        .vertex_count = @divExact(vertex_data.len, 2),
     };
+    app.initializeBuffers();
     return app;
 }
 
 pub fn deinit(self: *App) void {
+    self.vertex_buffer.release();
     self.gfx.destroy(self.allocator);
 
     zglfw.destroyWindow(self.window);
@@ -77,41 +87,23 @@ pub fn deinit(self: *App) void {
     self.allocator.destroy(self);
 }
 
-pub fn run(self: *App) !void {
-    var buffer_desc = zgpu.wgpu.BufferDescriptor{
+fn initializeBuffers(self: *App) void {
+    const buffer_desc = zgpu.wgpu.BufferDescriptor{
         .label = "Some GPU-side data buffer",
-        .usage = .{ .copy_dst = true, .copy_src = true },
-        .size = 16,
+        .usage = .{ .copy_dst = true, .vertex = true },
+        .size = vertex_data.len * @sizeOf(f32),
         .mapped_at_creation = .false,
     };
-    const buffer1 = self.gfx.device.createBuffer(buffer_desc);
-    defer buffer1.release();
-
-    buffer_desc.label = "Output buffer";
-    buffer_desc.usage = .{ .copy_dst = true, .map_read = true };
-    buffer_desc.size = 16;
-    buffer_desc.mapped_at_creation = .false;
-    const buffer2 = self.gfx.device.createBuffer(buffer_desc);
-    defer buffer2.release();
+    self.vertex_buffer = self.gfx.device.createBuffer(buffer_desc);
 
     // First submit the write operation
-    const numbers = try self.allocator.alloc(u8, 16);
-    defer self.allocator.free(numbers);
-    for (0..16) |i| {
-        numbers[i] = @as(u8, @intCast(i));
-    }
-    self.gfx.queue.writeBuffer(buffer1, 0, u8, numbers);
+    self.gfx.queue.writeBuffer(self.vertex_buffer, 0, f32, &vertex_data);
+}
 
-    var context = Context{
-        .ready = false,
-        .buffer = buffer2,
-    };
-
+pub fn run(self: *App) !void {
     // Main render loop
     while (!self.window.shouldClose()) {
-        // while (!context.ready) {
         self.gfx.device.tick();
-        // }
         zglfw.pollEvents();
 
         const view = self.gfx.swapchain.getCurrentTextureView();
@@ -119,8 +111,6 @@ pub fn run(self: *App) !void {
 
         const encoder = self.gfx.device.createCommandEncoder(null);
         defer encoder.release();
-
-        encoder.copyBufferToBuffer(buffer1, 0, buffer2, 0, 16);
 
         const color_attachment = [_]zgpu.wgpu.RenderPassColorAttachment{.{
             .view = view,
@@ -138,7 +128,9 @@ pub fn run(self: *App) !void {
         const pipeline = self.gfx.lookupResource(self.pipeline) orelse unreachable;
 
         pass.setPipeline(pipeline);
-        pass.draw(3, 1, 0, 0);
+
+        pass.setVertexBuffer(0, self.vertex_buffer, 0, vertex_data.len * @sizeOf(f32));
+        pass.draw(self.vertex_count, 1, 0, 0);
 
         pass.end();
         pass.release();
@@ -150,27 +142,13 @@ pub fn run(self: *App) !void {
         _ = self.gfx.present();
 
         // After submitting commands, we can map the buffer to read its contents
-        buffer2.mapAsync(
-            .{ .read = true },
-            0,
-            16,
-            onBuffer2Mapped,
-            @as(?*anyopaque, @constCast(&context)),
-        );
-
-        // Wait until the callback sets ready to true
-        while (!context.ready) {
-            self.gfx.device.tick();
-        }
-
-        // Now that we know it's mapped (callback was called), we can safely read it
-        if (context.buffer.getConstMappedRange(u8, 0, 16)) |data| {
-            std.debug.print("Buffer contents: {any}\n", .{data});
-        }
-        // Unmap after reading
-        context.buffer.unmap();
-        // Reset ready flag for next frame
-        context.ready = false;
+        // buffer2.mapAsync(
+        //     .{ .read = true },
+        //     0,
+        //     16,
+        //     onBuffer2Mapped,
+        //     @as(?*anyopaque, @constCast(&context)),
+        // );
 
         self.window.swapBuffers();
     }
@@ -199,12 +177,24 @@ fn createPipeline(gctx: *zgpu.GraphicsContext) zgpu.RenderPipelineHandle {
         .write_mask = .all,
     }};
 
+    const position_attribute = zgpu.wgpu.VertexAttribute{
+        .shader_location = 0,
+        .format = .float32x2,
+        .offset = 0,
+    };
+
+    const vertex_buffer_layout = zgpu.wgpu.VertexBufferLayout{
+        .array_stride = 2 * @sizeOf(f32),
+        .attribute_count = 1,
+        .attributes = &[_]zgpu.wgpu.VertexAttribute{position_attribute},
+    };
+
     const pipeline_desc = zgpu.wgpu.RenderPipelineDescriptor{
         .vertex = .{
             .module = shader_module,
             .entry_point = "vs_main",
-            .buffer_count = 0,
-            .buffers = null,
+            .buffer_count = 1,
+            .buffers = &[_]zgpu.wgpu.VertexBufferLayout{vertex_buffer_layout},
         },
         .primitive = .{
             .topology = .triangle_list,
@@ -221,10 +211,4 @@ fn createPipeline(gctx: *zgpu.GraphicsContext) zgpu.RenderPipelineHandle {
     };
 
     return gctx.createRenderPipeline(pipeline_layout, pipeline_desc);
-}
-
-fn onBuffer2Mapped(status: zgpu.wgpu.BufferMapAsyncStatus, userdata: ?*anyopaque) callconv(.c) void {
-    const ctx: *Context = @ptrCast(@alignCast(userdata));
-    ctx.ready = true;
-    std.debug.print("Buffer2 mapped: {any}\n", .{status});
 }
